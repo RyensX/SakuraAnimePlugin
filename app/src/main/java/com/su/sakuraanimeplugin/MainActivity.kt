91bacd3ca6aaca37fb2902ccec2d094087c9af7b
Ryensu <ryensu@qq.com>
2022-02-17 00:51:40 +0800

    [feature]添加一个关于Activity（示例展示页面）

--- app/src/main/java/com/su/sakuraanimeplugin/MainActivity.kt
+++ app/src/main/java/com/su/sakuraanimeplugin/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.su.sakuraanimeplugin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.su.sakuraanimeplugin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.pluginInfo.text = "樱花动漫MediaBox插件"
+        binding.pluginInfo.text = "这是樱花动漫MediaBox插件的主界面"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.add("关于")?.apply {
+            intent = Intent(this@MainActivity, OtherActivity::class.java)
+        }
+        return true
     }
 }
