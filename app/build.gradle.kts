798e495a94db46c3e967e9dbbbf0b6be4fce67d5
RyensX <ryensu@qq.com>
2022-04-05 20:22:45 +0800

    [chore]Release 1.4(5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.su.sakuraanimeplugin"
         minSdk = 21
         targetSdk = 31
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
